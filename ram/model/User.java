a54a5285e6c8b8ee0a6ab26d493fc531d4de3a5e
Ramkumar G <ramkumar.g.0420@gmail.com>
2021-06-17 18:17:27 +0530

    User model added

---
+++ ram/model/User.java
@@ -0,0 +1,13 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	String name;
+	String email, password;
+}
